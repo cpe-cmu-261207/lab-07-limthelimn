394fafe8afbc7210552d2845979063b69fd715ec
limthelimn <nuttawan.lim@gmail.com>
2021-09-05 19:18:07 +0700

    all done

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -5,12 +5,12 @@ const Canvas = () => {
   const state = PixelPainterStore.useState()
 
   return (
-    <table className="ring-2 ring-gray-200 rounded-md mx-auto">
+    <table className = "ring-2 ring-gray-200 rounded-md mx-auto">
       <tbody>
         {state.canvas.map((row, i) =>
-          <tr key={i}>
+          <tr key = {i}>
             {row.map((cell, j) =>
-              <Cell x={j} y={i} key={`${j}${i}`} />)}
+              <Cell x = {j} y = {i} key = {`${j}${i}`} />)}
           </tr>
         )}
       </tbody>
